500b6b04c85eb82d07585ad5c2aea9290d16a335
timo <e0000370@u.nus.edu>
2015-08-16 20:08:29 +0800

    ex01 by tangmeng

--- CSExercises/Ex01-06/Ex01.cs
+++ CSExercises/Ex01-06/Ex01.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Write a program that would request for your name and print a greeting like this:
+//Good Morning Venkat
+//where Venkat is the name you had input.
 namespace CSExercises
 {
     public class Ex01
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            
+            string name;
+            Console.WriteLine("Please input your name:");
+            name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Good Morning  " + name);
         }
     }
 }
